6cea424355d60c0fa95c2ebb66efef773e354f74
Bernardo Ayala <ba@bernardoayala.com>
2020-09-30 11:46:07 -0400

    look for python files in subdirectories

--- createBat/createBat.py
+++ createBat/createBat.py
@@ -1,12 +1,12 @@
 import os
 
 root = os.getcwd()
-files = os.listdir(root)
 
-for file in files:
-    if file.endswith(".py"):
-        fileName = os.path.splitext(file)[0]
-        filePath = root + "\\" + file
-        batFile = open(fileName + ".bat", "w+")
-        batFile.write("@py.exe " + filePath + " %*")
-        batFile.close()
+for path, dirs, files in os.walk(root):
+    for file in files:
+        if file.endswith(".py"):
+            fileName = os.path.splitext(file)[0]
+            filePath = root + "\\" + file
+            batFile = open(fileName + ".bat", "w+")
+            batFile.write("@py.exe " + filePath + " %*")
+            batFile.close()
